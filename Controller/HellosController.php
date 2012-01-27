1e4db9759bca9885ecefc54814530dddfa4efd1d
Roland Schuetz <mail@rolandschuetz.at>
2012-01-27 21:32:02 +0100

    Added sample for exposing controller methods

---
+++ Controller/HellosController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Hello Controller
+ *
+ */
+class HellosController extends AppController {
+
+/**
+ * return for a specific time and a specific name a greeting
+ *
+ * @banchaRemotable
+ */
+	public function getGreeting($timestamp,$firstname) {
+
+		// ignore Judas
+		if($firstname=="Judas") {
+			// Bancha will form the appropriete request
+			return false;
+		}
+
+		// look for the day time
+		$hour = date("H",$timestamp);
+		if($hour<12) {
+			$msg = "Good morning, ".$firstname."!";
+		} elseif($hour>19) {
+			$msg = "Good evening, ".$firstname."!";
+		} else {
+			$msg = "Hallo ".$firstname."!";
+		}
+		
+		// return in extjs format
+		return array(
+			'success' => true,
+			'data' => $msg,
+		);
+	}
+	
+}
