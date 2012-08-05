d0ed93520ca2f50b9bc787d7b803807f7f0f8455
Roland Schuetz <mail@rolandschuetz.at>
2012-08-04 23:37:35 -0600

    Added Bancha translation example html

---
+++ webroot/js/localization-sample.js
@@ -0,0 +1,85 @@
+/*!
+ *
+ * Bancha Project : Combining Ext JS and CakePHP (http://banchaproject.org)
+ * Copyright 2011-2012 Roland Schuetz, Kung Wong, Andreas Kern, Florian Eckerstorfer
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       Bancha
+ * @copyright     Copyright 2011-2012 Roland Schuetz, Kung Wong, Andreas Kern, Florian Eckerstorfer
+ * @link          http://banchaproject.org Bancha Project
+ * @since         Bancha v 0.9.2
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @author        Roland Schuetz <mail@rolandschuetz.at>
+ * @version       Bancha v PRECOMPILER_ADD_RELEASE_VERSION
+ *
+ * For more information go to http://banchaproject.org
+ */
+/*jslint browser: true, vars: true, undef: true, nomen: true, eqeq: false, plusplus: true, bitwise: true, regexp: true, newcap: true, sloppy: true, white: true */
+/*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, immed:true, latedef:true, newcap:true, noarg:true, noempty:true, regexp:true, undef:true, trailing:false */
+/*global Ext:false, Bancha:false, window:false */
+
+// API and Bancha is already included,
+// now load sample dependencies
+Ext.require([
+    'Ext.panel.Panel'
+]);
+
+
+Ext.onReady(function() {
+
+    // every time a button is clicked just translate the text below
+    var translate = function(lang) {
+
+        // This is the only translation-specific
+        var text = Bancha.t('Inside JavaScript Bancha.t(str,langCode) will translate the text in the correct language.', lang)+'<br /><br/>';
+        text += Bancha.t('With the usage of following console tool you can collect all translation-ready strings:', lang);
+        text += '<br /><code>./Console/cake i18n extract</code><br /><br />';
+        text += Bancha.t('Bancha translations are handled like normal CakePHP translations, so just translate cakes pot files and you\'re ready to go.', lang)+'<br /><br />';
+        text += Bancha.t('Enjoy!', lang);
+
+        // update component text
+        Ext.getCmp('translations-panel').update(text);
+    };
+
+    // create a new panel
+    Ext.create('Ext.Panel', {
+        title: 'Bancha Localization',
+        renderTo: 'translation',
+        id: 'translations-panel',
+        width: 600,
+        height: 300,
+
+        dockedItems: [{
+            xtype: 'toolbar',
+            dock: 'top',
+            items: [{
+                xtype: 'label',
+                text: 'Choose your language: '
+            }, {
+                xtype: 'button',
+                text: 'English',
+                iconCls: 'icon-eng',
+                handler: Ext.Function.pass(translate, 'eng')
+            }, {
+                xtype: 'button',
+                text: 'German',
+                iconCls: 'icon-deu',
+                handler: Ext.Function.pass(translate, 'deu')
+            }, {
+                xtype: 'button',
+                text: 'French',
+                iconCls: 'icon-fre',
+                handler: Ext.Function.pass(translate, 'fre')
+            }]
+        }],
+        html: [ // this will always be in english
+            'Bancha provides already build in support for localizing your frontend.<br />',
+            'For a demo click on of the buttons above'
+            ].join('')
+    });
+
+});
+
+// eof
