c5357c6a4bcbe39d6bfff1cb193a9025e2b8848a
Roland Schuetz <mail@rolandschuetz.at>
2012-09-02 18:26:56 -0600

    Better phpdocs

--- Controller/HelloWorldsController.php
+++ Controller/HelloWorldsController.php
@@ -13,7 +13,11 @@ class HelloWorldsController extends AppController {
 	}
 
 	/**
+	 * Greets two people by name.
 	 * @banchaRemotable
+	 * @param String $name the first person
+	 * @param String $name2 the second person
+	 * @return String the greeting
 	 */
 	public function helloyou($name, $name2) {
 		return array('data' => sprintf('Hello %s and %s!', $name, $name2));
