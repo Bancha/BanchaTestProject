364ed5b745aad5eae2ca225382d6e9102f5c16df
Roland Schuetz <mail@rolandschuetz.at>
2012-09-02 18:28:26 -0600

    Updated error handling, see also https://github.com/Bancha/Bancha/pull/86

--- webroot/js/error-handling.js
+++ webroot/js/error-handling.js
@@ -9,7 +9,7 @@
  * @package       Bancha
  * @copyright     Copyright 2011-2012 Roland Schuetz, Kung Wong, Andreas Kern, Florian Eckerstorfer
  * @link          http://banchaproject.org Bancha Project
- * @since         Bancha v 0.9.0
+ * @since         Bancha v 0.9.2
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  * @author        Roland Schuetz <mail@rolandschuetz.at>
  * @version       Bancha v PRECOMPILER_ADD_RELEASE_VERSION
@@ -18,39 +18,65 @@
  */
 /*jslint browser: true, vars: true, undef: true, nomen: true, eqeq: false, plusplus: true, bitwise: true, regexp: true, newcap: true, sloppy: true, white: true */
 /*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, immed:true, latedef:true, newcap:true, noarg:true, noempty:true, regexp:true, undef:true, trailing:false */
-/*global Ext, Bancha */
+/*global Ext, Bancha, alert */
 
-// catch every debug exception thrown from either ExtJS or Bancha
-Ext.Error.handle = function(err) {
-    Ext.Msg.alert('Error', err.msg);
-};
-
-// catch server-side errors
-Ext.direct.Manager.on('exception', function(err){
-	// normalize ExtJS and Sencha Touch
-	var data = (typeof err.getCode === 'function') ? {
-		code: err.getCode(),
-		message: err.getMessage(),
-		data: {
-			msg: err.getData()
-		},
-
-		// bancha-specific
-		exceptionType: err.config.exceptionType,
-		where: err.config.where,
-		trace: err.config.trace
-	} : err;
-	
-	// handle error
-	if(data.code==="parse") {
-		// parse error
-		Ext.Msg.alert('Bancha: Server-Response can not be decoded',data.data.msg);
-	} else {
-		// exception from server
-		Ext.Msg.alert('Bancha: Exception from Server',
-			"<br/><b>"+(data.exceptionType || "Exception")+": "+data.message+"</b><br /><br />"+
-			((data.where) ? data.where+"<br /><br />Trace:<br />"+data.trace : "<i>Turn on the debug mode in cakephp to see the trace.</i>"));
-	}
-});
+(function() {
+    
+    var handler = function(err) {
+        var msg = (err.message || err.msg || err),
+            errorString = "",
+            name;
+        
+        
+        // ext not present
+        if(!window.Ext || !Ext.Msg || typeof Ext.Msg.alert !== 'function') {
+            alert("The ExtJS 4 library is not present. Please check installation instructions for further inforamtion.");
+            return;
+        }
+        
+        // try to encode whole object
+        try {
+            errorString = Ext.encode(err);
+        } catch(e) {
+            try {
+                if((e.message || e.msg) === "Maximum call stack size exceeded") {
+                    errorString = "{";
+                    for (name in err) {
+                        if (err.hasOwnProperty(name)) {
+                            if(typeof err[name] === 'function') {
+                                errorString += name + ': function(), ';
+                            } else {
+                                errorString += name + ': '+err[name] + ', ';
+                            }
+                        }
+                    }
+                    errorString = errorString+"}";
+                } else {
+                    errorString = "We could not encode the error object."+e.message;
+                }
+            } catch(e2) {
+                errorString = "An error in the error handler. Great^^ We could not encode the error object. (2)";
+            }
+        }
+        
+        // output error
+        msg = '<br/><span style="font-size:12px;">Gratulation, you just found an error ;-)<br /><br />'+
+              'Please report the error including following '+
+              'information on our <a href="https://github.com/Bancha/Bancha/issues">issue tracker</a>.<br/><br/>'+
+              'About the error: <br />'+msg+'<br />'+errorString+'<br /><br />';
+        try {
+            // display error
+            Ext.Msg.alert('Bancha: Unknown Error',msg);
+        } catch(e3) {
+            alert(msg);
+        }
+    };
+    
+    // overwrite error-handler.js error handling
+    Ext.Error.handle = handler;
+    
+    // overwrite browser error handler
+    window.onerror = handler;
+}());
 
 // eof
