6e6055057fdb12a712c728f30c8b278159a0e977
Roland Schuetz <mail@rolandschuetz.at>
2013-08-27 15:28:33 +0900

    Updated associations example

--- webroot/js/associations-sample.js
+++ webroot/js/associations-sample.js
@@ -16,9 +16,7 @@
  *
  * For more information go to http://banchaproject.org
  */
-/*jslint browser: true, vars: true, undef: true, nomen: true, eqeq: false, plusplus: true, bitwise: true, regexp: true, newcap: true, sloppy: true, white: true */
-/*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, immed:true, latedef:true, newcap:true, noarg:true, noempty:true, regexp:true, undef:true, trailing:false */
-/*global Ext, Example:true, Bancha */
+/*global Ext, Bancha */
 
 // include Bancha
 Ext.Loader.setConfig('enabled', true);
@@ -42,31 +40,83 @@ Ext.application({
          * Example 1 - Load all hasMany associations
          */
 
-        // load the user with the id 1
-        Bancha.getModel('User').load(1, {
-            callback: function(record, operation) {
+        // display a grid of articles
+        Ext.create('Ext.form.Panel', {
+            title: 'Select a user to load all his articles',
 
+            // The fields
+            defaultType: 'combobox',
+            items: [{
+                fieldLabel: 'User',
+                name: 'user',
+                store: {
+                    xtype: 'Ext.data.Store',
+                    model: 'Bancha.model.User',
+                    autoLoad: true
+                },
+                displayField: 'name',
+                valueField: 'id',
+                listeners: {
+                    change: function(field, newValue) {
+                        var articleCombo = field.up('form').down('[name=article]'),
+                            // get the selected user record
+                            userRecord = field.getStore().getById(newValue);
 
-                // the user is loaded, get all associated articles
-                var store = record.articles();
-                store.load();
+                        // activate the field below, if a user is selected
+                        articleCombo.setDisabled(!userRecord);
 
+                        // clear old selection
+                        articleCombo.setValue('');
 
-                // display a grid of the data
-                Ext.create('Ext.grid.Panel', {
-                    title: 'Display all to user 1 associated articles',
+                        if(!userRecord) {
+                            return; // no user selected
+                        }
 
-                    // grid configs
-                    store: store,
-                    columns: Ext.clone(Example.Article.gridColumns),
+                        // get the articles store from the association
+                        var store = userRecord.articles();
 
-                    // some additional styles
-                    height: 350,
-                    width: 650,
-                    frame: true,
-                    renderTo: 'has-many-grid'
-                });
-            }
+                        // load data for the new store
+                        store.load();
+
+                        // use this store in the combo below
+                        articleCombo.bindStore(store);
+                    }
+                }
+            },{
+                fieldLabel: 'User\'s Article',
+                name: 'article',
+                displayField: 'title',
+                valueField: 'id',
+                disabled: true, // activate after something is selected above
+                listeners: {
+                    change: function(field, newValue) {
+                        var article = field.getStore().getById(newValue);
+
+                        if(article) {
+                            Ext.Msg.alert('Selected Article', [
+                                'You have selected the article <em>'+
+                                article.get('title'),
+                                '</em>, written on ',
+                                Ext.Date.format(article.get('date'),'m/d/Y'),
+                                '.'
+                            ].join(''));
+                        }
+                    }
+                }
+            }],
+
+            // some additional styles
+            layout: 'anchor',
+            defaults: {
+                anchor: '100%',
+                queryMode: 'local',
+                padding: 10
+            },
+            padding: 10,
+            height: 150,
+            width: 650,
+            frame: true,
+            renderTo: 'has-many-combo'
         });
 
 
@@ -75,19 +125,19 @@ Ext.application({
          * Example 2 - Load all belongsTo associations
          */
 
-         // Since all examples share the same data, it's possible that
-         // a NotFoundExceptions get thrown because a user record got
-         // deleted in the crud examples. In this case show another msg
-         Ext.direct.Manager.on('exception', function() {
-                Ext.Msg.alert('Loaded belongsTo user record', [
-                    'The Article doesn\'t belong to any user.<br /><br />',
-                    'This can happen since we are using the same data in all examples.'
-                ].join(''));
+        // Since all examples share the same data, it's possible that
+        // a NotFoundExceptions get thrown because a user record got
+        // deleted in the crud examples. In this case show another msg
+        Ext.direct.Manager.on('exception', function() {
+            Ext.Msg.alert('Loaded belongsTo user record', [
+                'The Article doesn\'t belong to any user.<br /><br />',
+                'This can happen since we are using the same data in all examples.'
+            ].join(''));
         });
 
         // display a grid of articles
         var articles = Ext.create('Ext.data.Store', {
-            model: Bancha.getModel('Article'),
+            model: 'Bancha.model.Article',
             autoLoad: true
         });
         Ext.create('Ext.grid.Panel', {
@@ -95,7 +145,44 @@ Ext.application({
 
             // grid configs
             store: articles,
-            columns: Ext.clone(Example.Article.gridColumns),
+            columns: [{
+                flex: 1,
+                xtype: 'gridcolumn',
+                text: 'Title',
+                dataIndex: 'title',
+                field: {
+                    xtype: 'textfield',
+                    name: 'title'
+                }
+            }, {
+                flex: 1,
+                xtype: 'datecolumn',
+                text: 'Date',
+                dataIndex: 'date',
+                field: {
+                    xtype: 'datefield',
+                    name: 'date'
+                }
+            }, {
+                flex: 1,
+                xtype: 'gridcolumn',
+                text: 'Body',
+                dataIndex: 'body',
+                field: {
+                    xtype: 'textfield',
+                    name: 'body'
+                }
+            }, {
+                flex: 1,
+                xtype: 'booleancolumn',
+                text: 'Published',
+                dataIndex: 'published',
+                field: {
+                    uncheckedValue: false,
+                    xtype: 'checkboxfield',
+                    name: 'published'
+                }
+            }],
 
             // some additional styles
             height: 350,
@@ -109,8 +196,11 @@ Ext.application({
                     // load the user of the selected article
                     selected[0].getUser({
                         callback: function(user, operation) {
-                            Ext.Msg.alert('Loaded belongsTo user record',
-                                'The Article '+selected[0].get('title')+' belongs to the user '+(user.get('name') || '<i>No name defined</i>'));
+                            Ext.Msg.alert('Loaded belongsTo user record', [
+                                'The Article '+selected[0].get('title'),
+                                ' belongs to the user ',
+                                user.get('name') || '<i>No name defined</i>'
+                            ].join(''));
                         }
                     });
                 } //eo selectionchanged
@@ -118,75 +208,3 @@ Ext.application({
         });
     }
 }); //eo application
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * This object provides some basic ExtJS code
- * to make the sample above cleaner and more
- * focused on the important parts
- */
-var Example = {};
-
-/**
- * columsn for the article model
- */
-Example.Article = {};
-Example.Article.gridColumns = [
-    {
-        flex: 1,
-        xtype: "gridcolumn",
-        text: "Title",
-        dataIndex: "title",
-        field: {
-            xtype: "textfield",
-            name: "title"
-        }
-    }, {
-        flex: 1,
-        xtype: "datecolumn",
-        text: "Date",
-        dataIndex: "date",
-        field: {
-            xtype: "datefield",
-            name: "date"
-        }
-     }, {
-        flex: 1,
-        xtype: "gridcolumn",
-        text: "Body",
-        dataIndex: "body",
-        field: {
-            xtype: "textfield",
-            name: "body"
-        }
-     }, {
-        flex: 1,
-        xtype: "booleancolumn",
-        text: "Published",
-        dataIndex: "published",
-        field: {
-            uncheckedValue: false,
-            xtype: "checkboxfield",
-            name: "published"
-        }
-    }
-];
-
-
-// eof
